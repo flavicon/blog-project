3d2e073d473aafd87ef568d20deb2e30395e5d02
Flavio Silva <flavio.devjs@gmail.com>
2022-05-29 15:45:19 -0300

    create posts page

---
+++ src/components/Post/index.jsx
@@ -0,0 +1,9 @@
+const Post  = () => {
+    return (
+        <div>
+            <p>Posts</p>
+        </div>
+    );
+};
+
+export default Post;
---
+++ src/pages/posts.jsx
@@ -0,0 +1,7 @@
+import Post from '../components/Post';
+
+const Posts = () => {
+    return <Post />;
+};
+
+export default Posts;
